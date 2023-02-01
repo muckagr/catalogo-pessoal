4593de71bfbe08c194905c6c9c77389df256cf3c
muckagr <arthurafk@gmail.com>
2023-02-01 15:10:57 -0300

    refactoring index scrypt

--- scrypts/index.js
+++ scrypts/index.js
@@ -1,22 +1,13 @@
 function Slideshow(buttonObject) {
-  buttonClass = buttonObject.getAttribute("id");
-  buttonNumber = buttonClass.charAt(buttonClass.length - 1);
+  buttonInstruction = buttonObject.getAttribute("class");
+  buttonNumber = buttonObject
+    .getAttribute("id")
+    .charAt(buttonObject.getAttribute("id").length - 1);
   slidesContainer = document.getElementById("slide-container-" + buttonNumber);
-  prevButton = document.getElementById("btn-prev-" + buttonNumber);
-  nextButton = document.getElementById("btn-next-" + buttonNumber);
 
-  let click = 0;
-
-  if (click == 0) {
+  if (buttonInstruction == "next") {
     slidesContainer.scrollLeft += 340;
-  }
-
-  nextButton.addEventListener("click", () => {
-    slidesContainer.scrollLeft += 340;
-    click += 1;
-  });
-  prevButton.addEventListener("click", () => {
+  } else {
     slidesContainer.scrollLeft -= 340;
-    click += 1;
-  });
+  }
 }
