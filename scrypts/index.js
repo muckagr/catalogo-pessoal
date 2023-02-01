32e0f3c566696d625df43721bb2ab5d4a37a85c9
muckagr <arthurafk@gmail.com>
2023-02-01 14:23:07 -0300

    swaping ids and classes

--- scrypts/index.js
+++ scrypts/index.js
@@ -1,16 +1,22 @@
 function Slideshow(buttonObject) {
-  buttonClass = buttonObject.getAttribute("class");
+  buttonClass = buttonObject.getAttribute("id");
   buttonNumber = buttonClass.charAt(buttonClass.length - 1);
-  slidesContainer = document.getElementsByClassName(
-    "slide-container-" + buttonNumber
-  )[0];
-  prevButton = document.getElementsByClassName("btn-prev-" + buttonNumber)[0];
-  nextButton = document.getElementsByClassName("btn-next-" + buttonNumber)[0];
+  slidesContainer = document.getElementById("slide-container-" + buttonNumber);
+  prevButton = document.getElementById("btn-prev-" + buttonNumber);
+  nextButton = document.getElementById("btn-next-" + buttonNumber);
+
+  let click = 0;
+
+  if (click == 0) {
+    slidesContainer.scrollLeft += 340;
+  }
 
   nextButton.addEventListener("click", () => {
     slidesContainer.scrollLeft += 340;
+    click += 1;
   });
   prevButton.addEventListener("click", () => {
     slidesContainer.scrollLeft -= 340;
+    click += 1;
   });
 }
